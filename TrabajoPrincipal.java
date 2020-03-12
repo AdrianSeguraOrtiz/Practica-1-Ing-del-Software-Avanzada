d1e5cb0ece0f0b7261087cb42e908c52d9df9a65
AdrianSeguraOrtiz <adrianseor.99@uma.es>
2020-03-12 15:59:16 +0100

    Ventana maximizada

---
+++ TrabajoPrincipal.java
@@ -0,0 +1,33 @@
+package trabajo;
+
+import java.awt.Frame;
+
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
+import trabajo.TrabajoControlador;
+import trabajo.TrabajoVistaPanel;
+
+public class TrabajoPrincipal {
+	public static void main(String[] args){
+		Runnable rr = new Runnable(){
+				public void run ( ) {
+					
+					JFrame ventana = new JFrame ("Ventana");
+					
+					TrabajoVistaPanel tvp = new TrabajoVistaPanel();
+					TrabajoControlador tc = new TrabajoControlador(tvp);
+					
+					tvp.getbBuscar().addActionListener(tc);
+					
+					ventana.getContentPane().add(tvp);
+					
+					ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+					ventana.pack();
+					ventana.setExtendedState(Frame.MAXIMIZED_BOTH);
+					ventana.setVisible(true);			
+				}
+		};
+		SwingUtilities.invokeLater(rr);
+	}
+}
